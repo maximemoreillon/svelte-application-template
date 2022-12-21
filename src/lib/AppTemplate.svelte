b39eec721ebf779f088ebf090849460600fb3131
Maxime Moreillon <m.moreillon@gmail.com>
2022-12-21 12:40:09 +0900

    started working on a login screen

--- src/lib/AppTemplate.svelte
+++ src/lib/AppTemplate.svelte
@@ -1,35 +1,73 @@
-<TopAppBar bind:this={topAppBar} variant="fixed">
-  <Row>
-    <Section>
-      <IconButton class="material-icons">menu</IconButton>
-      <Title>Fixed</Title>
-    </Section>
-    <Section align="end" toolbar>
-      <IconButton class="material-icons" aria-label="Download"
-        >file_download</IconButton
-      >
-      <IconButton class="material-icons" aria-label="Print this page"
-        >print</IconButton
-      >
-      <IconButton class="material-icons" aria-label="Bookmark this page"
-        >bookmark</IconButton
-      >
-    </Section>
-  </Row>
-</TopAppBar>
-<AutoAdjust {topAppBar}>
-    <slot></slot>
-</AutoAdjust>
+<Drawer variant="modal" fixed={false} bind:open>
+    <Header>
+      <DrawerTitle>{options.title}</DrawerTitle>
+      <Subtitle>{options.subtitle}</Subtitle>
+    </Header>
+    <Content>
+      <List>
+        <Item
+          href="javascript:void(0)">
+          <Graphic class="material-icons" aria-hidden="true">inbox</Graphic>
+          <Text>Inbox</Text>
+        </Item>
+      </List>
+    </Content>
+</Drawer>
+
+<Scrim fixed={false} />
+
+<AppContent class="app-content">
+    <TopAppBar bind:this={topAppBar} variant="fixed">
+    <Row>
+        <Section>
+            <IconButton class="material-icons" on:click={() => {open = !open}}>menu</IconButton>
+            <Title>{options.title}</Title>
+        </Section>
+        <Section align="end" toolbar>
+        <IconButton class="material-icons">logout</IconButton>
+        </Section>
+    </Row>
+    </TopAppBar>
+    <AutoAdjust {topAppBar}>
+        <Login options={options}/>
+        <!-- <slot></slot> -->
+
+    </AutoAdjust>
+</AppContent>
+
+
  
 
 <script lang="ts">
-  import TopAppBar, {
+
+import Login from './Login.svelte';
+
+
+import List, { Item, Text, Graphic, Separator, Subheader } from '@smui/list';
+import IconButton from '@smui/icon-button';
+import Drawer, {
+    AppContent,
+    Content,
+    Header,
+    Title as DrawerTitle,
+    Subtitle,
+    Scrim,
+} from '@smui/drawer';
+import Button, { Label } from '@smui/button';
+import TopAppBar, {
     Row,
     Section,
     Title,
     AutoAdjust,
-  } from '@smui/top-app-bar';
-  import IconButton from '@smui/icon-button';
+} from '@smui/top-app-bar';
  
-  let topAppBar: TopAppBar;
+let topAppBar: TopAppBar;
+let open = false;
+
+export const options = {
+    title: 'Untitled application',
+    subtitle: 'An untitled application',
+    login_url: 'https://api.users.maximemoreillon.com/v2/auth/login'
+}
+
 </script>
---
+++ src/lib/Login.svelte
@@ -0,0 +1,48 @@
+<form on:submit={login}>
+    <Textfield bind:value={credentials.username} label="Username">
+        <Icon class="material-icons" slot="leadingIcon">person</Icon>
+    </Textfield>
+    <Textfield bind:value={credentials.password} label="Password" type="password">
+        <Icon class="material-icons" slot="leadingIcon">key</Icon>
+    </Textfield>
+    <Button>
+        <ButtonIcon class="material-icons">login</ButtonIcon>
+        <Label>Login</Label>
+    </Button>
+</form>
+
+<script lang="ts">
+import Textfield from '@smui/textfield';
+import Icon from '@smui/textfield/icon';
+import Button, { Label, Icon as ButtonIcon} from '@smui/button';
+
+export let options: any
+
+const credentials = {
+    username: '',
+    password: ''
+}
+
+
+const login = async () => {
+
+    const url = options.login_url
+    const opts = { 
+        method: 'POST',
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        }, 
+        body: JSON.stringify(credentials)
+    }
+    try {
+        const res = await fetch(url, opts)
+        const data = await res.json()
+
+        console.log(data)
+    } catch (error) {
+        
+    }
+}
+
+</script>
